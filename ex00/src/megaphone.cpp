f158f313449c5d3f4eacfee9b4a15b2386fcc85f
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c2r2s7.42lisboa.com>
2023-08-30 18:57:47 +0100

    ex00 is done

---
+++ ex00/src/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 18:05:26 by abaiao-r          #+#    #+#             */
+/*   Updated: 2023/08/30 18:55:54 by abaiao-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    if(ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    else if(ac >= 1)
+    {
+        i = 1;
+        while(av[i])
+        {
+            j = 0;
+            while(av[i][j])
+            {
+                std::cout << (char)toupper(av[i][j]);
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
